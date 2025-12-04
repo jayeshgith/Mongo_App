13025673ad2c3247d297f1eaf74a838a981f11b9
Manish <manishkunthoor@gmail.com>
2025-12-04 09:51:59 +0530

    Updated MongoApp design3

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Chat = require("./models/chat.js");
 main()
@@ -8,36 +8,37 @@ main()
 
   .catch((err) => console.log(err));
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/MongoAPP");
-    
-
+  await mongoose.connect(
+    "mongodb+srv://manishkunthoor_db_user:JyU8KKVZMGYveuGO@mongoapp.ii8gstm.mongodb.net/TEST?appName=MongoAPP"
+  );
 }
-let allChats = [{
+let allChats = [
+  {
     from: "Mani",
     to: "Bob",
     message: "Hello, Bob! How are you?",
     created_at: new Date(),
-},
-{
+  },
+  {
     from: "shyam",
     to: "ram",
     message: "Hi, how are you man!.",
     created_at: new Date(),
-
-},
-{
+  },
+  {
     from: "shraddha",
     to: "jaya",
     message: "Glad to hear that! Want to catch up later?",
     created_at: new Date(),
-    }, {
+  },
+  {
     from: "jaya",
     to: "shraddha",
     message: "Sure, let's meet at the cafe at 5 PM.",
     created_at: new Date(),
-
-    }]
+  },
+];
 Chat.insertMany(allChats)
-
-    .then((res) => { console.log(res) });
-    
+.then((res) => {
+  console.log(res);
+});
